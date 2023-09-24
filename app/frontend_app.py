4021f3568708942bf4b458b925294c0bf7b8f7e1
Magnus <97634880+MagnusS0@users.noreply.github.com>
2023-09-24 16:04:25 +0200

    feat: Added streamlit frontend_app
    - Added streamlit frontend_app for a simple UI to interact with the model
    - Added streamlit to pyproject.toml

---
+++ app/frontend_app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import requests  # Import requests library
+
+# FastAPI server URL
+SERVER_URL = "http://localhost:8000/recommendations/"  # Replace with your FastAPI server URL
+
+st.title('Movie Recommendation System')
+
+# User Inputs
+query = st.text_input('Enter a movie title:')  # Create a search bar
+num_rec = st.number_input('Number of Recommendations:', min_value=1, value=10)  # Number of recommendations
+
+# Get Recommendations Button
+if st.button('Get Recommendations'):  # If the button is pressed
+    if query:  # If a query is entered
+        st.subheader('Recommendations for "{}":'.format(query))
+        
+        # Prepare payload
+        payload = {"movie": query, "num_rec": num_rec}
+        
+        try:
+            # Send POST request to FastAPI server
+            response = requests.post(SERVER_URL, json=payload)
+            
+            if response.status_code == 200:
+                recommendations = response.json()  # Parse JSON response
+                
+                if recommendations:
+                    # Display recommended movies
+                    st.write('Recommended Movies:')
+                    for rec in recommendations.get('recommendations', []):
+                        st.write('- ' + rec)
+                    
+                    # Display metrics
+                    metrics = recommendations.get('metrics', {})
+                    st.write('Metrics:')
+                    st.write(f"Popularity: {metrics.get('popularity', 'N/A')}")
+                    st.write(f"Average Vote: {metrics.get('vote_avg', 'N/A')}")
+                    st.write(f"Vote Count: {metrics.get('vote_count', 'N/A')}")
+                else:
+                    st.write('No recommendations available for this movie.')
+            
+            else:
+                st.write('Error occurred:', response.text)  # Display error message from server
+            
+        except Exception as e:
+            st.write('Error occurred:', e)  # In case of an error, display it
+    else:
+        st.write('Please enter a movie title.')
